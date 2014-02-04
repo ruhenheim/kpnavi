f2846b4f2d74b9eecccbfbe90f094c6d46bf4209
ruhenheim <asd.uchiyama@gmail.com>
2014-02-04 23:49:19 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kpnavi::Application.config.secret_key_base = '376f4fb025a001b88b18e85aa1b2e2ef4b0bf96245dffbbe6f19d72862ceb8b6196780a52d0a23d64466b75edecbbae6d09ee7aee3f55b5424146ac6ee2e65cb'
